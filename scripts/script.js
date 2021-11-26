46ab54b7f259c6cae2fa0d34546ee21ffb25cef6
rxndrmrnsc <ruxandraimarinescu@gmail.com>
2021-11-26 11:20:23 +0200

    solved onload error

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,3 @@
 var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-
-function displayCurrentDate(){
-    var sidebar = document.getElementById('sidebar');
-    sidebar.innerHtml = date;
-}
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
