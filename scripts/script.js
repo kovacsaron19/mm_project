2f6d29c5ee0ba88592c6bb2b9e7e700e8042e665
rxndrmrnsc <ruxandraimarinescu@gmail.com>
2021-11-28 15:39:49 +0200

    Added calendar display

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
-function calculateFirstDay(){
+
+const DaysEnums = { 0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday" }
+
+function calculateFirstDay(date) {
     // var day = cDate2.getDate();
     // console.log(day);
     // var month = cDate2.getMonth() + 1;
@@ -18,30 +21,61 @@ function calculateFirstDay(){
     // var weekday = (day + Math.floor(2.6*month - 0.2) - 2*century + year + Math.floor(year/4) + Math.floor(century/4)) % 7
 
     // console.log(weekday);
-    var date = new Date();
     var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    firstDay = firstDay.getDay();
+    if (firstDay == 0) {
+        firstDay = 7;
+    }
+    else {
+        firstDay -= 1;
+    }
     return firstDay;
 }
 
-function displayCalendar(date){
-    var firstWeekday = calculateFirstDay();
-    var grid = document.getElementById("mygrid2");
+function displayCalendar(date) {
+    let cellDate = 1;
+    var firstWeekday = calculateFirstDay(date);
     var year = date.getFullYear();
     var month = date.getMonth();
+    var lastDate = new Date(year, month+1, 0).getDate();
 
-    for (let i = 1; i < 6; i++) {
-        let j = 1;
-        if(i===1){
-            j = j + firstWeekday;
+    let children = document.getElementsByClassName("cell");
+    let child = children[0];
+    let count = 1;
+    let ok = true;
+
+    while (ok) {
+        if (child.classList.contains(DaysEnums[firstWeekday])) {
+            child.innerHTML = cellDate;
+            cellDate+=1;
+            child = children[count];
+            count += 1;
+            ok = false;
         }
-        for (j; j <= 7; j++) {
-            console.log(grid)
-            console.log(grid.rows[0].cells[0])
-            var cell = grid.rows[i].cells[j].elements[0];
-            var text = document.createTextNode(i*j);
-            cell.appendChild(text);
+        else {
+            child = children[count];
+            count += 1;
         }
     }
+    while (cellDate <= lastDate) {
+        child.innerHTML = cellDate;
+        cellDate += 1;
+        child = children[count];
+        count += 1;
+    }
+
+    // for (let i = 0; i < 6; i++) {
+    //     let j = 0;
+    //     if(i===0){
+    //         j = firstWeekday;
+    //     }
+    //     for (j; j <= 7; j++) {
+    //         console.log(grid)
+    //         console.log(grid.rows[0].cells[0])
+    //         var cell = grid.
+    //         cell.innerHtml = "a";
+    //     }
+    // }
 }
 
 
